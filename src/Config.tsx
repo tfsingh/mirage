e50852586b6bc41fd1d47b5746fd53e240802b55
Tej Singh <tejfsingh@gmail.com>
2024-03-12 08:44:51 -0700

    Fix width of appbar in config

--- src/Config.tsx
+++ src/Config.tsx
@@ -96,128 +96,134 @@ const Config = () => {
 
     return (
         <Container>
-            <AppBar position="static" color="default" sx={{
-                alignItems: 'center', justifyContent: 'center', width: '366px', maxWidth: '600px', margin: '0 auto', height: '50px', bgcolor: '#9ab08f'
-            }}>
-                <Typography variant="h6" gutterBottom sx={{ mt: 1.5, fontSize: '28px', color: '#3a4037' }}>
-                    new chat
-                </Typography>
-            </AppBar>
-
             <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-                <Box sx={{ backgroundColor: 'white', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'flex-start', p: 1, margin: '0 auto', maxWidth: '600px', width: 'auto', bgcolor: '#F2F1E9' }}>
-                    <Box sx={{ backgroundColor: 'white', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', mr: 2, p: 1, maxWidth: '650px', bgcolor: '#F2F1E9' }}>
-                        <FormControl component="form" onSubmit={handleSubmit} variant="standard" sx={{ width: '100%', bgcolor: '#F2F1E9' }}>
-                            <TextField
-                                label="chat name"
-                                variant="outlined"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                                fullWidth
-                                required
-                                size="small"
-                                sx={{ mb: 1 }}
-                                inputProps={{ maxLength: 20 }}
-                            />
-                            <TextField
-                                label="url"
-                                variant="outlined"
-                                value={url}
-                                onChange={handleUrlChange}
-                                fullWidth
-                                required
-                                size="small"
-                                sx={{ mb: 1 }}
-                            />
-                            <Tooltip
-                                title="unique links to scrape"
-                                placement="left"
-                                PopperProps={{
-                                    modifiers: [
-                                        {
-                                            name: 'offset',
-                                            options: {
-                                                offset: [0, -10],
-                                            },
-                                        },
-                                    ],
-                                }}
-                            >
-                                <TextField
-                                    label="depth"
-                                    type="number"
-                                    variant="outlined"
-                                    value={depth}
-                                    onChange={handleDepthChange}
-                                    fullWidth
-                                    required
-                                    size="small"
-                                    inputProps={{ min: 1, max: 300 }}
-                                    sx={{ mb: 1 }}
-                                />
-                            </Tooltip>
+                <Box sx={{ backgroundColor: 'white', maxWidth: '600px', margin: '0 auto', width: 'auto', bgcolor: '#F2F1E9' }}>
+                    <AppBar position="static" color="default" sx={{
+                        alignItems: 'center', justifyContent: 'center', width: '100%', bgcolor: '#9ab08f', height: '15%'
+                    }}>
+                        <Typography variant="h6" gutterBottom sx={{ mt: 1.5, fontSize: '28px', color: '#3a4037' }}>
+                            new chat
+                        </Typography>
+                    </AppBar>
 
-                            <Tooltip
-                                title="url to restrict scraping to"
-                                placement="left"
-                                PopperProps={{
-                                    modifiers: [
-                                        {
-                                            name: 'offset',
-                                            options: {
-                                                offset: [0, -10],
-                                            },
-                                        },
-                                    ],
-                                }}>
-                                <TextField
-                                    label="base url"
-                                    variant="outlined"
-                                    value={baseUrl}
-                                    onChange={handleBaseUrlChange}
-                                    fullWidth
-                                    size="small"
-                                    sx={{ mb: 0.5 }}
-                                />
-                            </Tooltip>
-                            <FormControlLabel
-                                control={<Checkbox size="small" checked={ignoreFragments} onChange={handleIgnoreFragmentsChange} />}
-                                label="ignore fragments"
-                                sx={{ mb: 0.5 }}
-                            />
+                    <Box sx={{ display: 'flex', justifyContent: 'center', bgcolor: '#F2F1E9' }}>
+                        <Box sx={{
+                            backgroundColor: 'white', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'flex-start', p: 1, width: '100%', bgcolor: '#F2F1E9'
+                        }}>
+                            < Box sx={{ backgroundColor: 'white', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', mr: 2, p: 1, bgcolor: '#F2F1E9' }}>
+                                <FormControl component="form" onSubmit={handleSubmit} variant="standard" sx={{ width: '100%', bgcolor: '#F2F1E9' }}>
+                                    <TextField
+                                        label="chat name"
+                                        variant="outlined"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                        fullWidth
+                                        required
+                                        size="small"
+                                        sx={{ mb: 1 }}
+                                        inputProps={{ maxLength: 20 }}
+                                    />
+                                    <TextField
+                                        label="url"
+                                        variant="outlined"
+                                        value={url}
+                                        onChange={handleUrlChange}
+                                        fullWidth
+                                        required
+                                        size="small"
+                                        sx={{ mb: 1 }}
+                                    />
+                                    <Tooltip
+                                        title="unique links to scrape"
+                                        placement="left"
+                                        PopperProps={{
+                                            modifiers: [
+                                                {
+                                                    name: 'offset',
+                                                    options: {
+                                                        offset: [0, -10],
+                                                    },
+                                                },
+                                            ],
+                                        }}
+                                    >
+                                        <TextField
+                                            label="depth"
+                                            type="number"
+                                            variant="outlined"
+                                            value={depth}
+                                            onChange={handleDepthChange}
+                                            fullWidth
+                                            required
+                                            size="small"
+                                            inputProps={{ min: 1, max: 300 }}
+                                            sx={{ mb: 1 }}
+                                        />
+                                    </Tooltip>
 
-                            <Button type="submit" variant="contained" color="primary" size="small"
-                                sx={{
-                                    bgcolor: '#9ab08f',
-                                    color: '#3a4037',
-                                    textTransform: 'lowercase',
-                                    fontSize: '18px',
-                                    '&:hover': {
+                                    <Tooltip
+                                        title="url to restrict scraping to"
+                                        placement="left"
+                                        PopperProps={{
+                                            modifiers: [
+                                                {
+                                                    name: 'offset',
+                                                    options: {
+                                                        offset: [0, -10],
+                                                    },
+                                                },
+                                            ],
+                                        }}
+                                    >
+                                        <TextField
+                                            label="base url"
+                                            variant="outlined"
+                                            value={baseUrl}
+                                            onChange={handleBaseUrlChange}
+                                            fullWidth
+                                            size="small"
+                                            sx={{ mb: 0.5 }}
+                                        />
+                                    </Tooltip>
+                                    <FormControlLabel
+                                        control={<Checkbox size="small" checked={ignoreFragments} onChange={handleIgnoreFragmentsChange} />}
+                                        label="ignore fragments"
+                                        sx={{ mb: 0.5 }}
+                                    />
+
+                                    <Button type="submit" variant="contained" sx={{
                                         bgcolor: '#9ab08f',
                                         color: '#3a4037',
-                                    }
-                                }}>
-                                submit
-                            </Button>
-                        </FormControl>
-                    </Box>
+                                        textTransform: 'lowercase',
+                                        fontSize: '18px',
+                                        '&:hover': {
+                                            bgcolor: '#9ab08f',
+                                            color: '#3a4037',
+                                        }
+                                    }}>
+                                        submit
+                                    </Button>
+                                </FormControl>
+                            </Box>
 
-                    <Box sx={{ backgroundColor: 'white', display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'center', p: 1, bgcolor: '#F2F1E9' }}>
-                        <FormGroup>
-                            <FormLabel component="legend">tags to scrape:</FormLabel>
-                            {['h1', 'h2', 'h3', 'p', 'code', 'li', 'table'].map((tag) => (
-                                <FormControlLabel
-                                    key={tag}
-                                    control={<Checkbox size="small" value={tag} checked={selectedTags.includes(tag)} onChange={handleTagChange} />}
-                                    label={tag}
-                                    sx={{ margin: 0, padding: 0 }}
-                                />
-                            ))}
-                        </FormGroup>
+                            <Box sx={{ backgroundColor: 'white', display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'center', p: 1, bgcolor: '#F2F1E9' }}>
+                                <FormGroup>
+                                    <FormLabel component="legend">tags to scrape:</FormLabel>
+                                    {['h1', 'h2', 'h3', 'p', 'code', 'li', 'table'].map((tag) => (
+                                        <FormControlLabel
+                                            key={tag}
+                                            control={<Checkbox size="small" value={tag} checked={selectedTags.includes(tag)} onChange={handleTagChange} />}
+                                            label={tag}
+                                            sx={{ margin: 0, padding: 0 }}
+                                        />
+                                    ))}
+                                </FormGroup>
+                            </Box>
+                        </Box>
                     </Box>
                 </Box>
-            </Box>
-        </Container>
+            </Box >
+        </Container >
     );
 };
 
