00c62f608e16b6c7072c86a8f99a9386af54e6e3
Tej Singh <tejfsingh@gmail.com>
2024-03-14 17:35:49 -0700

    Fix build issues

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['js-big-decimal'],
-  },
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: './src/main.jsx',
-    },
-  },
 });
